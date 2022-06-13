cdd22f6a4129e8330a213db6725bec076da7b859
Felipe Barreiros <felipewww@outlook.com.br>
2022-06-13 07:20:13 -0300

    add start-master and start-worker as a sample for docker entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin
 
-echo "Hello spark!"
+echo "Hello spark 2!"
 
 # hadoop SSH key
 # https://stackoverflow.com/questions/15195048/hadoop-require-roots-password-after-enter-start-all-sh
@@ -10,5 +10,8 @@ ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 
 /etc/init.d/ssh start
+
+start-master.sh
+start-worker.sh spark://0.0.0.0:7077
 
 tail -f /dev/null
